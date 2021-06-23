f6215a507d786b80958aa2338808307fd06e5856
Jamesrx33 <68870179+Jamesrx33@users.noreply.github.com>
2021-06-23 11:40:31 -0500

    Challenge Commit 1

--- Module Files/scraping.py
+++ Module Files/scraping.py
@@ -13,13 +13,16 @@ def scrape_all():
 
     news_title, news_paragraph = mars_news(browser)
 
+    img_dic = img_scrape(browser)
+
     # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "img_list": img_dic
     }
 
     # Stop webdriver and return data
@@ -35,7 +38,7 @@ def mars_news(browser):
     browser.visit(url)
 
     # Optional delay for loading the page
-    browser.is_element_present_by_css('div.list_text', wait_time=1)
+    browser.is_element_present_by_css('div.list_text', wait_time=10)
 
     # Convert the browser html to a soup object and then quit the browser
     html = browser.html
@@ -50,6 +53,7 @@ def mars_news(browser):
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
     except AttributeError:
+        print("Error in Mars_News Collection")
         return None, None
 
     return news_title, news_p
@@ -59,6 +63,9 @@ def featured_image(browser):
     # Visit URL
     url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     browser.visit(url)
+
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('button', wait_time=10)
 
     # Find and click the full image button
     full_image_elem = browser.find_by_tag('button')[1]
@@ -71,9 +78,10 @@ def featured_image(browser):
     # Add try/except for error handling
     try:
         # Find the relative image url
-        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+        img_url_rel = img_soup.find('img', class_='headerimage fade-in').get('src')
 
-    except AttributeError:
+    except AttributeError as e:
+        print(f"Error in Featured_Image Collection \n\n {e}")
         return None
 
     # Use the base url to create an absolute url
@@ -87,7 +95,8 @@ def mars_facts():
         # Use 'read_html' to scrape the facts table into a dataframe
         df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
 
-    except BaseException:
+    except BaseException as e:
+        print(f"----Error in Mars_Facts Collection ---- \n\n{e}")
         return None
 
     # Assign columns and set index of dataframe
@@ -95,7 +104,48 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html()
+
+def img_scrape(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # Loop once for each hemisphere
+    for x in range(4):
+        
+        # Find the click on the Image Link
+        imgs = browser.find_by_css("img.thumb", wait_time=3)[x]
+        imgs.click()
+        
+        # Parse browser HTML and drill down to find the HREF of the "Sample" link
+        html = browser.html
+        img_soup = soup(html, 'html.parser')
+        img_url_rel = img_soup.find("div", class_="downloads").ul.li.a.get('href')
+        
+        # Store the URL for the high-def picture
+        img_url = f'https://marshemispheres.com/{img_url_rel}'
+        
+        # Find and store the Title for the picture
+        img_title = img_soup.find("div", class_="cover").h2.text
+
+        # Append the URL and Title to our hemisphere_image_urls list
+        hemisphere_image_urls.append(
+        {
+            'img_url': img_url,
+            'title': img_title
+        }
+        )
+        
+        # Visit the original site for further processing
+        browser.visit(url)
+
+    # Return the hemisphere images list
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
